1be784058e2e4ea60ef8e0e283842e12a8efc597
Sidnei Pacheco <sidneip.junior@gmail.com>
2024-08-28 16:10:52 -0300

    chore: ci/cd staging teste

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/chatbot
+cd /home/ubuntu/chatbot || exit
+rvm use 3.3.3
 bundle install
 yarn install
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,9 +3,31 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo apt-get install -y curl gpg build-essential
 
-if ! rvm -v &>/dev/null; then
+# Verifica se o RVM está instalado
+
+FILE="~/.rvm/scripts/rvm"
+
+# Check if the file exists
+if [ -f "$FILE" ]; then
+  # Source the file
+  source "$FILE"
+else
+  echo "File $FILE does not exist."
+fi
+
+if ! command -v rvm &>/dev/null; then
+  echo "RVM não está instalado. Instalando agora..."
+
+  # Importa as chaves GPG necessárias para instalar o RVM
   gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+  # Baixa e instala o RVM
   \curl -sSL https://get.rvm.io | bash -s stable
+
+  # Carrega o RVM no shell atual
+  source ~/.rvm/scripts/rvm
+else
+  echo "RVM já está instalado."
 fi
 
 source ~/.rvm/scripts/rvm
