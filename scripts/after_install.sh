18c176343ba92c68e997a6e8b3505ba2b3aebf34
Sidnei Pacheco <sidneip.junior@gmail.com>
2024-08-28 16:13:37 -0300

    chore: ci/cd staging teste

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 cd /home/ubuntu/chatbot || exit
+
+if [ -f "$HOME/.bash_profile" ]; then
+  source "$HOME/.bash_profile"
+elif [ -f "$HOME/.bashrc" ]; then
+  source "$HOME/.bashrc"
+fi
+
 rvm use 3.3.3
 bundle install
 yarn install
