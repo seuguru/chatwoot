3fc64395688091115065dbfe4d1c07b4a5d987b2
Sidnei Pacheco <sidneip.junior@gmail.com>
2024-08-28 16:53:54 -0300

    chore: ci/cd staging teste

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
-source "$HOME/.bashrc"
-rvm use 3.3.3
-nvm use 20.15
-cd /home/ubuntu/chatbot || exit
+if [ -f "$HOME/.bash_profile" ]; then
+  source "$HOME/.bash_profile"
+elif [ -f "$HOME/.bashrc" ]; then
+  source "$HOME/.bashrc"
+fi
+
+# Load RVM into a shell session as a function
+if [ -s "$HOME/.rvm/scripts/rvm" ]; then
+  source "$HOME/.rvm/scripts/rvm"
+else
+  echo "RVM is not installed."
+  exit 1
+fi
+
+# Load NVM into a shell session as a function
+if [ -s "$HOME/.nvm/nvm.sh" ]; then
+  source "$HOME/.nvm/nvm.sh"
+else
+  echo "NVM is not installed."
+  exit 1
+fi
+
 sudo chown -R ubuntu:ubuntu /home/ubuntu/chatbot
 sudo chmod -R u+w /home/ubuntu/chatbot
+
+rvm use 3.3.3
+nvm use 20.15
+
+cd /home/ubuntu/chatbot || exit
 export RAILS_ENV=production
 export PORT=4001
 source ./scripts/set_env.sh
