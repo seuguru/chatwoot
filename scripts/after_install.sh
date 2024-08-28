30d3f988675d72821a51b0d1c0fc7c40b1e4490d
Sidnei Pacheco <sidneip.junior@gmail.com>
2024-08-28 16:45:21 -0300

    chore: ci/cd staging teste

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 cd /home/ubuntu/chatbot || exit
 
-source "$HOME/.bashrc"
+# Source the user's profile to ensure environment variables are loaded
+if [ -f "$HOME/.bash_profile" ]; then
+  source "$HOME/.bash_profile"
+elif [ -f "$HOME/.bashrc" ]; then
+  source "$HOME/.bashrc"
+fi
+
+# Load RVM into a shell session as a function
+if [ -s "$HOME/.rvm/scripts/rvm" ]; then
+  source "$HOME/.rvm/scripts/rvm"
+else
+  echo "RVM is not installed."
+  exit 1
+fi
+
+# Load NVM into a shell session as a function
+if [ -s "$HOME/.nvm/nvm.sh" ]; then
+  source "$HOME/.nvm/nvm.sh"
+else
+  echo "NVM is not installed."
+  exit 1
+fi
 
 rvm use 3.3.3
 nvm use 20.15
