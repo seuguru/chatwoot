1664aff75f9cc9127db00b13ec305ab6d02f3f5e
Sidnei Pacheco <sidneip.junior@gmail.com>
2024-09-02 19:48:45 -0300

    chore: ci/cd staging teste

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source ./scripts/set_version_manager.sh
 
 rvm use 3.3.3
 nvm use 20.15
@@ -7,11 +6,13 @@ nvm use 20.15
 cd /home/ubuntu/chatbot || exit
 export RAILS_ENV=production
 export PORT=4001
+source ./scripts/set_version_manager.sh
 source ./scripts/set_env.sh
 
 bundle exec rails db:migrate
 bundle exec rails assets:precompile
 kill_process_on_port $PORT
 kill_process_by_name "sidekiq"
-bundle exec rails ip_lookup:setup && bin/rails server -p $PORT -e $RAILS_ENV
+# bundle exec rails ip_lookup:setup && bin/rails server -p $PORT -e $RAILS_ENV
+bundle exec puma -C config/puma.rb
 bundle exec rails ip_lookup:setup && bundle exec sidekiq -C config/sidekiq.yml
