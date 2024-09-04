f53f3fc86118250a3ebb89c57a009bc83d8b6f8e
Sidnei Pacheco <sidneip.junior@gmail.com>
2024-09-04 16:42:58 -0300

    chore: fix

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -14,3 +14,5 @@ kill_process_on_port $PORT
 kill_process_by_name "sidekiq"
 bundle exec rails ip_lookup:setup && bin/rails server -p $PORT -e $RAILS_ENV >./log/production.log 2>&1 &
 bundle exec rails ip_lookup:setup && bundle exec sidekiq -C config/sidekiq.yml -e $RAILS_ENV >./log/sidekiq.log 2>&1 &
+
+echo "Deployment script completed."
